6acc3e88dac1120643234402935c717fea5868ee
qusai sallhab <qusaisallhab@gmail.com>
2024-10-31 10:49:17 +0200

    lastUpDate

--- randomNumberGenerator.cs
+++ randomNumberGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class randomNumberGenerator : MonoBehaviour
@@ -7,12 +8,21 @@ public class randomNumberGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        while(true)
+        {
+            int randomNumber= Random.Range(0,21);
+            if(randomNumber==5)continue;
+            if(randomNumber==15) break;
+            Debug.Log(randomNumber);
+            
+        }
         
     }
 }
